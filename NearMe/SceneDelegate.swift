b2fbca661092cc6abe2c00c6ee9d44ddbb3491ee
Yianni1980 <ioagian1980@gmail.com>
2023-01-10 15:49:30 +0200

    Initial commit - mapView creation

--- NearMe/SceneDelegate.swift
+++ NearMe/SceneDelegate.swift
@@ -17,8 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
-        guard let WindowScene  = (scene as? UIWindowScene) else { return }
+        guard let windowScene  = (scene as? UIWindowScene) else { return }
         let vc  = ViewController()
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController  = vc
+        window.makeKeyAndVisible()
+        self.window = window
+        
 
     }
 
--- NearMe/ViewController.swift
+++ NearMe/ViewController.swift
@@ -6,13 +6,66 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    var locationManager:CLLocationManager?
+    
+    lazy var mapView:MKMapView = {
+       let map = MKMapView()
+        map.showsUserLocation = true
+        map.translatesAutoresizingMaskIntoConstraints = false
+    
+        return map
+    }()
+    lazy var searchTextfield : UITextField = {
+        let searchTextfield = UITextField()
+        searchTextfield.layer.cornerRadius = 10
+        searchTextfield.clipsToBounds = true
+        searchTextfield.backgroundColor = UIColor.white
+        searchTextfield.placeholder = "Search"
+        searchTextfield.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        searchTextfield.translatesAutoresizingMaskIntoConstraints = false
+        return searchTextfield
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor  = .cyan
+        
+        //initialize location Manager
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.requestLocation()
+        
+        setupUI()
+    }
+    
+    private func setupUI () {
+        view.addSubview(searchTextfield)
+
+        view.addSubview(mapView)
+        
+        view.bringSubviewToFront(searchTextfield)
+
+        
+        //add contraints to the searchTextField
+        searchTextfield.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        searchTextfield.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchTextfield.widthAnchor.constraint(equalToConstant: view.bounds.size.width/1.2).isActive = true
+        searchTextfield.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
+        searchTextfield.returnKeyType  = .go
+        
+        
+        //add constraints to the mapView
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
     }
 
 
