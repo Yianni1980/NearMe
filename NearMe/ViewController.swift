46bccdb9637a8417eb4e661eec2d2b54266a9d5a
Yianni1980 <ioagian1980@gmail.com>
2023-01-10 15:13:59 +0200

    Initial Commit

---
+++ NearMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearMe
+//
+//  Created by ioannis on 10/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor  = .cyan
+    }
+
+
+}
+
